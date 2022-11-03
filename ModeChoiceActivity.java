0af89c58faf2f798fe6dc820dc63d26a3f6993b8
dfeda <90071399+dfeda@users.noreply.github.com>
2022-11-03 16:06:53 -0500

    Add files via upload

---
+++ ModeChoiceActivity.java
@@ -0,0 +1,36 @@
+package com.example.securucar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class ModeChoiceActivity extends AppCompatActivity {
+    private Button autonomousButton;
+    private Button controlledButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_mode_choice);
+
+        autonomousButton = (Button) findViewById(R.id.autonomousButton);
+        controlledButton = (Button) findViewById(R.id.controlledButton);
+        autonomousButton.setOnClickListener(v -> openAutonomousActivity());
+        controlledButton.setOnClickListener(v -> openControlledActivity());
+    }
+
+    public void openAutonomousActivity() {
+        Intent intent = new Intent(this, AutonomousActivity.class);
+        startActivity(intent);
+    }
+
+    public void openControlledActivity() {
+        Intent intent = new Intent(this, ControlledActivity.class);
+        startActivity(intent);
+    }
+}
